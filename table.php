f1335adfcdcbc464fd405e33f68dff555c985300
Programador Michel Rosales <95501615+Michprosal003@users.noreply.github.com>
2022-09-13 02:12:55 -0500

    Add files via upload

---
+++ table.php
@@ -0,0 +1,203 @@
+
+
+<link rel="stylesheet" href="table.css">
+
+<p>Tabla de Multiplicar</p>
+<div class="grid-container"> 
+
+
+
+<!--------Tabla del 1----> 
+<div class="grid-items"> 
+
+<?php 
+
+for($count=1; $count <=9; $count++)
+{
+
+echo("1 x " .$count. " = " .$count *1);
+echo("<br>");
+}
+
+
+?>
+
+</div>
+
+
+<!--------Tabla del 2----> 
+<div class="grid-items"> 
+
+<?php 
+
+for($count=1; $count <=9; $count++)
+{
+
+echo("2 x " .$count. " = " .$count *2);
+echo("<br>");
+}
+
+
+?>
+
+</div>
+
+
+
+
+<!--------Tabla del 3----> 
+<div class="grid-items"> 
+
+<?php 
+
+for($count=1; $count <=9; $count++)
+{
+
+echo("3 x " .$count. " = " .$count *3);
+echo("<br>");
+}
+
+
+?>
+
+</div>
+
+
+<!--------Tabla del 4----> 
+<div class="grid-items"> 
+
+<?php 
+
+for($count=1; $count <=9; $count++)
+{
+
+echo("4 x " .$count. " = " .$count *4);
+echo("<br>");
+}
+
+
+?>
+
+</div>
+
+
+<!--------Tabla del 5----> 
+<div class="grid-items"> 
+
+<?php 
+
+for($count=1; $count <=9; $count++)
+{
+
+echo("5 x " .$count. " = " .$count *5);
+echo("<br>");
+}
+
+
+?>
+
+</div>
+
+
+
+<!--------Tabla del 6----> 
+<div class="grid-items"> 
+
+<?php 
+
+for($count=1; $count <=9; $count++)
+{
+
+echo("6 x " .$count. " = " .$count *6);
+echo("<br>");
+}
+
+
+?>
+
+</div>
+
+
+
+<!--------Tabla del 7----> 
+<div class="grid-items"> 
+
+<?php 
+
+for($count=1; $count <=9; $count++)
+{
+
+echo("7 x " .$count. " = " .$count *7);
+echo("<br>");
+}
+
+
+?>
+
+</div>
+
+
+<!--------Tabla del 8----> 
+<div class="grid-items"> 
+
+<?php 
+
+for($count=1; $count <=9; $count++)
+{
+
+echo("8 x " .$count. " = " .$count *8);
+echo("<br>");
+}
+
+
+?>
+
+</div>
+
+
+<!--------Tabla del 9----> 
+<div class="grid-items"> 
+
+<?php 
+
+for($count=1; $count <=9; $count++)
+{
+
+echo("9 x " .$count. " = " .$count *9);
+echo("<br>");
+}
+
+
+?>
+
+</div>
+
+
+<!--------Tabla del 10----> 
+<div class="grid-items"> 
+
+<?php 
+
+for($count=1; $count <=9; $count++)
+{
+
+echo("10 x " .$count. " = " .$count *10);
+echo("<br>");
+}
+
+
+?>
+
+</div>
+
+
+
+
+
+
+
+
+
+
+</div>
+
